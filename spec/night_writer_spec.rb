205b43bfc9dcc78cd3cfddef6f2e3a65cbbbda76
Melony Franchini <meltravelz@gmail.com>
2023-01-14 11:36:12 -0700

    Setup simplecov

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,5 +1,4 @@
-require './lib/night_writer'
-# require_relative './spec_helper' ???
+require_relative 'spec_helper' 
 
 RSpec.describe NightWriter do 
   let(:night_writer) { NightWriter.new }
@@ -19,7 +18,7 @@ RSpec.describe NightWriter do
     # could stub this method if using attr_reader: 
     # allow(night_writer).to receive(:read_file).and_return('./message.txt')
     # allow(night_writer).to receive(:write_file).and_return('./braille.txt')
-    night_writer.call
+    night_writer.read_from_write_to
 
     expect(night_writer.read_file).to eq('./message.txt')
     expect(night_writer.write_file).to eq('./braille.txt')
