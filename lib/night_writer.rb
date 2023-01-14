932f6e56280dc8a1f378c537d15d560e01d4997d
Melony Franchini <meltravelz@gmail.com>
2023-01-14 12:24:01 -0700

    Delete extra spaces

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,5 @@
 class NightWriter
   attr_accessor :read_file, :write_file
-    # when running ruby/runner maybe use this: 
-    # attr_reader :read_file, :write_file
 
   def initialize
     @read_file = ARGV[0]
@@ -10,20 +8,21 @@ class NightWriter
 
   def read_from_write_to
     incoming_file = File.open(@read_file, "r")
-    # outgoing_file = File.new(@write_file, "w")
     
     text = incoming_file.read
     character_count = text.chars.count
-  
     puts "Created #{@write_file} contains #{character_count} characters"
-    # # require 'pry'; binding.pry
 
     File.write(@write_file, text)
   end
+
+
 end
 
-# below is acting like a runner file: 
+###### RUNNER #####
 # comment out when running rspec!
+# below is acting like a runner file: 
 
 # night_writer = NightWriter.new
 # night_writer.read_from_write_to
+
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -2,8 +2,6 @@ require_relative 'spec_helper'
 
 RSpec.describe NightWriter do 
   let(:night_writer) { NightWriter.new }
-  # this is saying: These are the 2 arguments 
-  # I expect to see in the command line
 
   before do
     night_writer.read_file = './message.txt'
@@ -15,9 +13,6 @@ RSpec.describe NightWriter do
   end
 
   it "has attributes" do
-    # could stub this method if using attr_reader: 
-    # allow(night_writer).to receive(:read_file).and_return('./message.txt')
-    # allow(night_writer).to receive(:write_file).and_return('./braille.txt')
     night_writer.read_from_write_to
 
     expect(night_writer.read_file).to eq('./message.txt')
