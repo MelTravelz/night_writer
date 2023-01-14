5351929b607919a9d486d01109f78d142dd948fb
Melony Franchini <meltravelz@gmail.com>
2023-01-14 11:25:23 -0700

    Add #initialize attributes test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,11 +1,31 @@
 class NightWriter
+  attr_accessor :read_file, :write_file
+    # when running ruby/runner maybe use this: 
+    # attr_reader :read_file, :write_file
 
   def initialize
-    @read_file = ARVG[0]
+    @read_file = ARGV[0]
     @write_file = ARGV[1]
-
   end
 
-  require 'pry'; binding.pry
+  def read_from_write_to
+    incoming_file = File.open(@read_file, "r")
+    # outgoing_file = File.new(@write_file, "w")
+    
+    text = incoming_file.read
+    character_count = text.chars.count
+  
+    
+    puts "Created #{@write_file} contains #{character_count} characters"
+    # # require 'pry'; binding.pry
 
+    File.write(@write_file, text)
+
+  end
 end
+
+# below is acting like a runner file: 
+# comment out when running rspec!
+
+# night_writer = NightWriter.new
+# night_writer.read_from_write_to
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,11 +1,28 @@
 require './lib/night_writer'
+# require_relative './spec_helper' ???
 
 RSpec.describe NightWriter do 
-  let(:night_writer) { NightWriter.new(ARGV[0], ARVG[1]) }
-  # this is saying it expects to see 2 arguments in the command line
+  let(:night_writer) { NightWriter.new }
+  # this is saying: These are the 2 arguments 
+  # I expect to see in the command line
+
+  before do
+    night_writer.read_file = './message.txt'
+    night_writer.write_file = './braille.txt'
+  end
 
   it "exists" do
     expect(night_writer).to be_instance_of(NightWriter)
+  end
+
+  it "has attributes" do
+    # could stub this method if using attr_reader: 
+    # allow(night_writer).to receive(:read_file).and_return('./message.txt')
+    # allow(night_writer).to receive(:write_file).and_return('./braille.txt')
+    night_writer.call
+
+    expect(night_writer.read_file).to eq('./message.txt')
+    expect(night_writer.write_file).to eq('./braille.txt')
   end
 
 end
