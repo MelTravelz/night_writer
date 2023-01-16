6ce1d305a7f728d872a4cc0432814f5a9a50b1c7
Melony Franchini <meltravelz@gmail.com>
2023-01-15 21:44:18 -0700

    Expand tests to show different functionality

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe NightWriter do
   before do
     night_writer.read_file = "./message.txt"
     night_writer.write_file = "./braille.txt"
+    night_writer.read_eng_write_brl
   end
 
   describe "#initialize" do
@@ -14,8 +15,6 @@ RSpec.describe NightWriter do
     end
 
     it "has attributes" do
-      night_writer.read_eng_write_brl
-
       expect(night_writer.read_file).to eq("./message.txt")
       expect(night_writer.write_file).to eq("./braille.txt")
       expect(night_writer.eng_brl_alphabet).to be_instance_of(Hash)
@@ -24,25 +23,22 @@ RSpec.describe NightWriter do
   end
 
   describe "#translate_eng_to_brl" do
-    it "can translate eng to brl" do
-      night_writer.read_eng_write_brl
-
+    it "can translate one eng character to brl" do
       expect(night_writer.translate_eng_to_brl("a")).to eq("0.\n..\n..")
-      expect(night_writer.translate_eng_to_brl("abc")).to eq("0.0.00\n..0...\n......")
-      expect(night_writer.translate_eng_to_brl("a b c")).to eq("0...0...00\n....0.....\n..........")
+    end
+    
+    it "can translate more than one eng character to brl" do
+    expect(night_writer.translate_eng_to_brl("abc")).to eq("0.0.00\n..0...\n......")
+    expect(night_writer.translate_eng_to_brl("a b c")).to eq("0...0...00\n....0.....\n..........")
     end
 
-    # it "can print multiple eng letters to brl letters" do
-    #   night_writer.read_eng_write_brl
-
-    #   expect(night_writer.translate_message("abc")).to eq("0.000.\n.....0\n......")
-    #   expect(night_writer.translate_message("a b c")).to eq("0...00..0.\n.........0\n..........")
+    # xit "can wrap brl letters to another line if over 40 eng characters in length" do
+    #   expected = 
+    #   expect(night_writer.translate_message("the quick brown fox jumps over the lazy dog")).to eq(expected)
     # end
 
     # xit "can return message if special character/capital letter is included in message.txt" do
-    #   night_writer.read_eng_write_brl
     #   allow(night_writer).to receive(:read_file).and_return("Hel-lo!")
-
     #   expect(night_writer.read_eng_write_brl).to eq("Your message contains invalid characters. Ensure you have ONLY lowercase english letters.")
     # end
   end
