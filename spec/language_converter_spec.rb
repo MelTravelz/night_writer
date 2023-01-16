fe04810f29d3448b03b0eef9e9c22bf2c85c7482
Melony Franchini <meltravelz@gmail.com>
2023-01-16 13:30:13 -0700

    Relocate tests from NightWriter to LanguageConverter where logic now lives

--- spec/language_converter_spec.rb
+++ spec/language_converter_spec.rb
@@ -23,49 +23,53 @@ RSpec.describe LanguageConverter do
   end 
 
   describe "Translate English to Braille" do
-    it "should #translate_eng_to_brl" do
-      expected = "0...0...00\n....0.....\n.........."
-      expect(language_converter.translate_eng_to_brl("a b c")).to eq(expected)
+    it "should #translate_eng_to_brl for one eng character" do
+      expect(night_writer.translate_eng_to_brl("a")).to eq("0.\n..\n..")
     end
 
-    it "should translate capital eng letters to brl" do
-      expected = "0.0.00..00000.\n..0........0.0\n........000000"
-      expect(language_converter.translate_eng_to_brl("ABC XYZ")).to eq(expected)
+    it "should #translate_eng_to_brl for more than one eng character" do
+      result = "0...0...00\n....0.....\n.........."
+      expect(language_converter.translate_eng_to_brl("a b c")).to eq(result)
     end
 
-    it "should delete all unknown characters and translate eng to brl" do
-      expected = "0.0.00..00000.\n..0........0.0\n........000000"
-      expect(language_converter.translate_eng_to_brl("abc! xyz?")).to eq(expected)
+    it "should #translate_eng_to_brl & wrap brl letters if over 40 eng characters long" do
+      result = ".00.0...000..0000...0.0.0..000..000.00...00.0000.0..0.0.0.0....00.0...0.0.0.00..\n"+ 
+        "0000.0..00..0.......0.00.000.0..0..0..........0.0....00..000..0000.0..0....0.0..\n"+
+        "0.......0.00....0.....0.0..00.....0.00....000.0.0...0.00..0...0.......0...0000..\n"+ 
+        "\n000.00\n.0.000\n..0..."
+      expect(language_converter.translate_eng_to_brl("the quick brown fox jumps over the lazy dog")).to eq(result)
+    end
+
+    it "should #translate_eng_to_brl & downcasing all capital letters" do
+      result = "0.0.00..00000.\n..0........0.0\n........000000"
+      expect(language_converter.translate_eng_to_brl("aBc xYz")).to eq(result)
+    end
+
+    it "should #translate_eng_to_brl & delete all unknown characters" do
+      result = "0.0.00..00000.\n..0........0.0\n........000000"
+      expect(language_converter.translate_eng_to_brl("abc! xyz?")).to eq(result)
     end
 
     it "should #create_array_of_eng_strings" do
-      expected = ["a", "b", "c", " ", "x", "y", "z"]
-      expect(language_converter.create_array_of_eng_strings("abc xyz")).to eq(expected)
+      result = ["a", "b", "c", " ", "x", "y", "z"]
+      expect(language_converter.create_array_of_eng_strings("abc xyz")).to eq(result)
     end
 
     it "should #create_brl_array" do
-      expected = [
+      result = [
         ["0.", "..", ".."], ['0.', '0.', '..'], ['00', '..', '..'], ['..', '..', '..'],
         ['00', '..', '00'], ['00', '.0', '00'], ['0.', '.0', '00']
       ]
-      expect(language_converter.create_brl_array(["a", "b", "c", " ", "x", "y", "z"])).to eq(expected)
+      expect(language_converter.create_brl_array(["a", "b", "c", " ", "x", "y", "z"])).to eq(result)
     end
-
-    # it "should also delete all nil/unknown characters within #create_brl_array" do
-    #   expected = [
-    #     ["0.", "..", ".."], ['0.', '0.', '..'], ['00', '..', '..'], ['..', '..', '..'],
-    #     ['00', '..', '00'], ['00', '.0', '00'], ['0.', '.0', '00']
-    #   ]
-    #   expect(language_converter.create_brl_array(["a", "b", "c", "!", " ", "x", "y", "z", "?"])).to eq(expected)
-    # end
 
     it "should #create_braille_row_string" do
       brl_array_argument = [
         ["0.", "..", ".."], ['0.', '0.', '..'], ['00', '..', '..'], ['..', '..', '..'],
         ['00', '..', '00'], ['00', '.0', '00'], ['0.', '.0', '00']
       ]
-      expected = "0.0.00..00000.\n..0........0.0\n........000000"
-      expect(language_converter.create_braille_row_string(brl_array_argument)).to eq(expected)
+      result = "0.0.00..00000.\n..0........0.0\n........000000"
+      expect(language_converter.create_braille_row_string(brl_array_argument)).to eq(result)
     end
   end
 
@@ -76,27 +80,26 @@ RSpec.describe LanguageConverter do
 
     it "should #create_array_of_brl_strings" do
       brl_text_argument = "0.0.00..00000.\n..0........0.0\n........000000"
-      expected =  ["0.0.00..00000.", "..0........0.0", "........000000"]
-      expect(language_converter.create_array_of_brl_strings(brl_text_argument)).to eq(expected)
+      result =  ["0.0.00..00000.", "..0........0.0", "........000000"]
+      expect(language_converter.create_array_of_brl_strings(brl_text_argument)).to eq(result)
     end
 
     it "should #create_no_extra_rows_array" do
       extra_row_argument =  ["0.0.00..00000.", "", "..0........0.0", "", "........000000"]
-      expected =  ["0.0.00..00000.", "..0........0.0", "........000000"]
-      expect(language_converter.create_no_extra_rows_array(extra_row_argument)).to eq(expected)
+      result =  ["0.0.00..00000.", "..0........0.0", "........000000"]
+      expect(language_converter.create_no_extra_rows_array(extra_row_argument)).to eq(result)
     end
 
     it "should #create_brl_row_by_index_array" do
       no_extra_rows_argument =  ["0.0.00..00000.", "..0........0.0", "........000000"]
-      expected = [ [ ["0.", "0.", "00", "..", "00", "00", "0."],["..", "0.", "..", "..", "..", ".0", ".0"],["..", "..", "..", "..", "00", "00", "00"] ] ]
-      expect(language_converter.create_brl_row_by_index_array(no_extra_rows_argument)).to eq(expected)
+      result = [ [ ["0.", "0.", "00", "..", "00", "00", "0."],["..", "0.", "..", "..", "..", ".0", ".0"],["..", "..", "..", "..", "00", "00", "00"] ] ]
+      expect(language_converter.create_brl_row_by_index_array(no_extra_rows_argument)).to eq(result)
     end
 
     it "create_eng_row_string(brl_row_by_index_array)" do
       triple_array_argument = [ [ ["0.", "0.", "00", "..", "00", "00", "0."],["..", "0.", "..", "..", "..", ".0", ".0"],["..", "..", "..", "..", "00", "00", "00"] ] ]
       expect(language_converter.create_eng_row_string(triple_array_argument)).to eq("abc xyz")
     end
-
   end
 end
 
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -21,29 +21,4 @@ RSpec.describe NightWriter do
       expect(night_writer.eng_brl_alphabet["a"]).to eq(["0.", "..", ".."])
     end
   end
-
-  describe "#translate_eng_to_brl" do
-    it "can translate one eng character to brl" do
-      expect(night_writer.translate_eng_to_brl("a")).to eq("0.\n..\n..")
-    end
-    
-    it "can translate more than one eng character to brl" do
-    expect(night_writer.translate_eng_to_brl("abc")).to eq("0.0.00\n..0...\n......")
-    expect(night_writer.translate_eng_to_brl("a b c")).to eq("0...0...00\n....0.....\n..........")
-    end
-
-    it "can wrap brl letters to another line if over 40 eng characters in length" do
-      expected = ".00.0...000..0000...0.0.0..000..000.00...00.0000.0..0.0.0.0....00.0...0.0.0.00..\n"+ 
-        "0000.0..00..0.......0.00.000.0..0..0..........0.0....00..000..0000.0..0....0.0..\n"+
-        "0.......0.00....0.....0.0..00.....0.00....000.0.0...0.00..0...0.......0...0000..\n"+ 
-        "\n000.00\n.0.000\n..0..."
-      expect(night_writer.translate_eng_to_brl("the quick brown fox jumps over the lazy dog")).to eq(expected)
-    end
-
-    # xit "can return message if special character/capital letter is included in message.txt" do
-    #   allow(night_writer).to receive(:read_file).and_return("Hel-lo!")
-    #   expect(night_writer.read_eng_write_brl).to eq("Your message contains invalid characters. Ensure you have ONLY lowercase english letters.")
-    # end
-  end
-
 end
